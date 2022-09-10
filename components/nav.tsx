682eb29279ce03fcdf0f54e71859cc52f361c438
Andy Garcia <andy.w.garcia@gmail.com>
2022-09-10 14:54:54 -0700

    Adding material ui and fixed nav bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,14 +1,29 @@
+import { Box } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 
 export default function Nav() {
   return (
-    <div style={{ display: "grid", gap: "4px" }}>
+    <Box
+      display="flex"
+      gap={2}
+      height="4rem"
+      component="nav"
+      justifyContent="flex-end"
+      alignItems="center"
+      position="fixed"
+      top="0"
+      left="0"
+      width="1"
+      overflow="hidden"
+      px="2rem"
+      bgcolor="white"
+    >
       <Link href="/">Home</Link>
       <Link href="/distinctive-framing">Distinctive Framing</Link>
       <Link href="/needlework-specialist">Needlework Specialist</Link>
       <Link href="/unique-touch">A Unique Touch</Link>
       <Link href="/quality">Quality</Link>
-    </div>
+    </Box>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,68 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import Image from "next/image";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import styles from "../styles/Home.module.css";
+import "../styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Nav from "../components/nav";
+import { Box } from "@mui/material";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Heidi&apos;s Place Custom Picture Framing</title>
+        <meta
+          name="description"
+          content="Heidi's Place Custom Picture Framing"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Nav />
+        <div className={styles.container}>
+          <Box component="main" className={styles.main}>
+            <Component {...pageProps} />
+          </Box>
+          <footer className={styles.footer}>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{" "}
+              <span className={styles.logo}>
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  width={72}
+                  height={16}
+                />
+              </span>
+            </a>
+          </footer>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
